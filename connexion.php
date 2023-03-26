0dd302db14c3ef3a5091fb8b95625a270c903e82
geoffray7700 <coorevits_geoffray@hotmail.com>
2023-03-26 14:32:12 +0200

    initialisation

---
+++ connexion.php
@@ -0,0 +1,13 @@
+
+<?php
+
+
+$user = 'eleve';
+$pass = 'eleve';
+
+try {
+    $db = new PDO('mysql:host=localhost;port=3307;dbname=opticontact', $user, $pass);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
